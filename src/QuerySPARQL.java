47125ca8e0f6500c4d5c3f305b10fc19fad3aeb5
Henrique <hfr@gmx.co.uk>
2020-03-18 16:43:07 -0300

    First

---
+++ src/QuerySPARQL.java
@@ -0,0 +1,39 @@
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.impl.SimpleLiteral;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.http.HTTPRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+
+public class QuerySPARQL {
+    private static Logger logger =
+            LoggerFactory.getLogger(QuerySPARQL.class);
+
+    public static void main(String args[]){
+        Repository repo = new HTTPRepository("http://localhost:7200/","19");
+        repo.init();
+        RepositoryConnection con = repo.getConnection();
+
+        TupleQuery query = con.prepareTupleQuery(QueryLanguage.SPARQL, "");
+        TupleQueryResult result = null;
+
+        result = query.evaluate();
+        while (result.hasNext()){
+            BindingSet binding = result.next();
+
+            Value name = binding.getValue("name");
+            logger.trace("name  = " + name.stringValue());
+            con.close();
+        }
+    }
+
+}
