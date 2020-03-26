ef4f62a2ff08d23c46797536a1d362a0476cd785
Henrique <hfr@gmx.co.uk>
2020-03-26 08:33:21 -0300

    Load from file

--- src/QuerySPARQL.java
+++ src/QuerySPARQL.java
@@ -1,4 +1,9 @@
 
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.http.HTTPRepository;
@@ -13,6 +18,7 @@ public class QuerySPARQL {
             LoggerFactory.getLogger(QuerySPARQL.class);
 
     public static void main(String args[]) {
+        //inicializa o repositório
         Repository repo = new HTTPRepository("http://192.168.1.102:7200/", "22");
         repo.init();
         RepositoryConnection con = repo.getConnection();
@@ -20,7 +26,7 @@ public class QuerySPARQL {
         String line=null;
         BufferedReader buf = null;
 
-
+        //Carrega as consultas do arquivo
         try {
             stream = new FileInputStream(new File("dbpedia.txt"));
         } catch (FileNotFoundException e) {
@@ -32,6 +38,7 @@ public class QuerySPARQL {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //Constroi a string com o conteúdo do arquivo
         StringBuilder sb = new StringBuilder();
 
 
@@ -45,30 +52,30 @@ public class QuerySPARQL {
         }
 
         String fileAsString = sb.toString();
-        System.out.println("Contents : " + fileAsString);
+        //System.out.println("Contents : " + fileAsString);
 
 
+        //Realiza a consulta
+        TupleQuery query = con.prepareTupleQuery(QueryLanguage.SPARQL, fileAsString);
+        TupleQueryResult result = null;
 
-        //TupleQuery query = con.prepareTupleQuery(QueryLanguage.SPARQL, "");
-        //TupleQueryResult result = null;
+         result = query.evaluate();
 
-        // result = query.evaluate();
+        while (result.hasNext()){
+            BindingSet binding = result.next();
 
-//        while (result.hasNext()){
-//            BindingSet binding = result.next();
-//
-//            Value subject = binding.getValue("subject");
-//            Value predicative = binding.getValue("predicative");
-//            Value object = binding.getValue("object");
-//            Value classeConceito = binding.getValue("ClasseConceito");
-//            //logger.trace("name  = " + name.stringValue());
-//            System.out.println("********");
-//            System.out.println(classeConceito.stringValue());
-//            //System.out.println(predicative.stringValue());
-//            //System.out.println(object.stringValue());
-//            System.out.println("********");
-//
-//        }
+            Value subject = binding.getValue("subject");
+            Value predicative = binding.getValue("predicative");
+            Value object = binding.getValue("object");
+            Value classeConceito = binding.getValue("ClasseConceito");
+            //logger.trace("name  = " + name.stringValue());
+            System.out.println("********");
+            System.out.println(classeConceito.stringValue());
+            //System.out.println(predicative.stringValue());
+            //System.out.println(object.stringValue());
+            System.out.println("********");
+
+        }
         con.close();
     }
 
